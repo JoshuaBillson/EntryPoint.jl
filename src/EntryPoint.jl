7ee02e09897ce75f584edb0c1d9ee3b8f32675df
JoshuaBillson <jmbillson@outlook.com>
2022-08-31 01:47:16 -0600

    Fixed Bug Regarding Caller's Filename

--- src/EntryPoint.jl
+++ src/EntryPoint.jl
@@ -2,8 +2,10 @@ module EntryPoint
 export @entrypoint
 
 macro entrypoint(exp)
+    line_number_node = eval(QuoteNode(__source__)) |> Symbol |> String
+    calling_file = split(line_number_node, ":")[1][4:end]
     quote
-        if $(esc(abspath(PROGRAM_FILE))) == $(esc(@__FILE__))
+        if $(esc(abspath(PROGRAM_FILE))) == $calling_file
             $(esc(exp))()
         end
     end
