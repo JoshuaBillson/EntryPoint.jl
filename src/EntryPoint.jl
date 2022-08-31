8b9adee40179192de0a8c93cabe30eb087f1e677
JoshuaBillson <jmbillson@outlook.com>
2022-08-31 01:19:23 -0600

    Fixed Scoping Bug

--- src/EntryPoint.jl
+++ src/EntryPoint.jl
@@ -1,10 +1,10 @@
 module EntryPoint
 export @entrypoint
 
-macro entrypoint(f)
+macro entrypoint(exp)
     quote
-        if abspath(PROGRAM_FILE) == @__FILE__
-            $f()
+        if $(esc(abspath(PROGRAM_FILE))) == $(esc(@__FILE__))
+            $(esc(exp))()
         end
     end
 end
